e3497927b2278d78107a46a1aacf7b7cc1766b03
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-03-10 15:07:45 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+
+document.querySelector('.guess').value = 10;
