343e93208a95e1b72cae30833c2c47ab9e4cd829
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-03-17 23:02:22 +0600

    Event listener added

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+/*document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 15;
 
 document.querySelector('.guess').value = 10;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🏴‍☠️ NO Number!';
+  }
+});
