315360f975ba7635bb12b7dc95bcad68a59174c6
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-03-26 15:45:51 +0600

    game logic implemented

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ document.querySelector('.score').textContent = 15;
 document.querySelector('.guess').value = 10;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = '🏴‍☠️ NO Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '🌋 Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '👀 Too Low!';
   }
 });
