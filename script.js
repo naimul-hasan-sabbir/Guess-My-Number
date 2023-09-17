aa85e3ab691f6a2d24bd2c1e680c3bd8cdb7b2df
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2023-09-17 11:41:39 +0600

    highscore feature added

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.querySelector('.guess').value = 10;
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20 
+let highscore = 0
 
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -22,6 +23,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if(score > highscore){
+      highscore = score
+      document.querySelector('.highscore').textContent = highscore
+    }
+
   } else if (guess > secretNumber) {
 
     if(score > 1){
@@ -48,15 +55,15 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function() {
-  score : 20;
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   document.querySelector('.message').textContent = 'Start Guessing...'
 
-  document.querySelector('.score').textContent = score
+  document.querySelector('.score').textContent = 0
   document.querySelector('.number').textContent = '?'
 
-  document.querySelector('.guess').textContent = ' '
+  document.querySelector('.guess').textContent = ''
 
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
