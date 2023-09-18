0a9ba874292342ce010a1e4640430135bbdf3a0e
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2023-09-18 12:39:15 +0600

    refactoring

--- script.js
+++ script.js
@@ -29,10 +29,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore
     }
 
-  } else if (guess > secretNumber) {
-
+  }else if(guess !== secretNumber){
     if(score > 1){
-      document.querySelector('.message').textContent = '🌋 Too High!';
+      document.querySelector('.message').textContent = guess > secretNumber ? '🌋 Too High!' : '👀 Too Low!';
       score--;
       document.querySelector('.score').textContent = score;
     }
@@ -40,18 +39,30 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = "You  Lost The Game!!!"
       document.querySelector('.score').textContent = 0;
     }
+  } 
+  // else if (guess > secretNumber) {
+
+  //   if(score > 1){
+  //     document.querySelector('.message').textContent = '🌋 Too High!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   }
+  //   else{
+  //     document.querySelector('.message').textContent = "You  Lost The Game!!!"
+  //     document.querySelector('.score').textContent = 0;
+  //   }
     
-  } else if (guess < secretNumber) {
-    if(score > 1){
-      document.querySelector('.message').textContent = '👀 Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    }else{
-      document.querySelector('.message').textContent = "You  Lost The Game!!!"
-      document.querySelector('.score').textContent = 0;
-    }
+  // } else if (guess < secretNumber) {
+  //   if(score > 1){
+  //     document.querySelector('.message').textContent = '👀 Too Low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   }else{
+  //     document.querySelector('.message').textContent = "You  Lost The Game!!!"
+  //     document.querySelector('.score').textContent = 0;
+  //   }
     
-  }
+  // }
 });
 
 document.querySelector('.again').addEventListener('click', function() {
