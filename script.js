5ce039420eeb29639cc396b8b8511c08f6095d9f
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2023-09-19 14:55:23 +0600

    refactoring

--- script.js
+++ script.js
@@ -12,14 +12,19 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20 
 let highscore = 0
 
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '🏴‍☠️ NO Number!';
+    //document.querySelector('.message').textContent = '🏴‍☠️ NO Number!';
+    displayMessage('🏴‍☠️ NO Number!')
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    //document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!')
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -31,12 +36,14 @@ document.querySelector('.check').addEventListener('click', function () {
 
   }else if(guess !== secretNumber){
     if(score > 1){
-      document.querySelector('.message').textContent = guess > secretNumber ? '🌋 Too High!' : '👀 Too Low!';
+      //document.querySelector('.message').textContent = guess > secretNumber ? '🌋 Too High!' : '👀 Too Low!';
+      displayMessage(guess > secretNumber ? '🌋 Too High!' : '👀 Too Low!')
       score--;
       document.querySelector('.score').textContent = score;
     }
     else{
-      document.querySelector('.message').textContent = "You  Lost The Game!!!"
+      //document.querySelector('.message').textContent = "You  Lost The Game!!!"
+      displayMessage("You  Lost The Game!!!")
       document.querySelector('.score').textContent = 0;
     }
   } 
@@ -69,7 +76,8 @@ document.querySelector('.again').addEventListener('click', function() {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start Guessing...'
+  //document.querySelector('.message').textContent = 'Start Guessing...'
+  displayMessage('Start Guessing...')
 
   document.querySelector('.score').textContent = 0
   document.querySelector('.number').textContent = '?'
